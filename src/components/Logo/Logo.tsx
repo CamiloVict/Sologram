12febf6c972d21d699ba61724d4d4a09435bfc17
Camilo Victoria <cvictoria@vovsolutions.com>
2022-11-22 12:01:22 -0500

    logo component implemented, styles unused files deleted

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,7 @@
+import { Link } from './style'
+
+export const Logo = (): JSX.Element => {
+  return (
+    <Link to="/" className='logo'><h1>Sologram</h1></Link>
+  )
+}
---
+++ src/components/Logo/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { Link as LinkRouter } from "react-router-dom";
+
+export const Link = styled(LinkRouter)`
+
+  @import url("https://fonts.googleapis.com/css?family=Oleo+Script");
+
+  text-decoration: none;
+  color: #1f69ff;
+  font-family: "Oleo Script", cursive;
+  font-size: 24px;
+  text-align: center;
+  background-color: white;
+`;
